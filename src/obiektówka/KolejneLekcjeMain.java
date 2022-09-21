1b2c54b55d3b14d0c337026d49f2496c90b05fae
firstName lastName <firstName.lastName@wp.pl>
2022-09-21 14:37:02 +0200

    Futures Subiects to be Continued ...

---
+++ src/obiektówka/KolejneLekcjeMain.java
@@ -0,0 +1,40 @@
+package obiektówka;
+
+public class KolejneLekcjeMain {
+    public static void main(String[] args) {
+        //Listy , Sety, HashSet, Dictonary,  TreeSet
+
+        //Pętla foreach
+
+
+
+        //Agregacja
+
+
+
+        // Interface
+
+
+
+
+
+        // Paradygmaty Programowania
+
+            //1. Dziedziczenie
+            //2. Polimormfizm
+            //3. Enkapsulacja
+            //4. Abstrakcja
+
+
+
+
+        // Zasady SOLID
+
+
+
+        //Programowanie Obiektowe Wyższego Poziomu II
+
+
+
+    }
+}
